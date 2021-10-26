5c1c7d940a8407c7d88a37e6440f8151f29b0600
felipegomescr <felipe.gomes@comprarapida.co>
2021-10-26 10:39:22 -0300

    Move /client to /

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
-import { Link } from "wouter";
+import useSWR from "swr";
+import { fetcher } from "../helpers";
+import { Posts } from "../components/Posts";
+import { Post } from "../types";
 
 const HomePage = () => {
+  const { data, error } = useSWR<Post[]>(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
+
+  if (error) {
+    return <p>Failed to load.</p>;
+  }
+
   return (
     <>
-      <h1>Home Page</h1>
-      <nav>
-        <Link href="/client">
-          <a>Client</a>
-        </Link>
-      </nav>
+      <h1>Client Page</h1>
+      {data ? <Posts data={data} /> : <p>Loading...</p>}
     </>
   );
 };
